4aa282c1d8516ef1eef46364b284657ec1bfd6fc
ashikjen200 <35518899+ashikjen200@users.noreply.github.com>
2024-03-04 16:34:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 module "ec2" {
   source  = "app.terraform.io/aws-Account1/ec2/aws"
-  version = "1.1.3"
+  version = "1.1.2"
+  ami = var.ami
+  instance_type = var.instance_type
+  tags = {
+   Name = var.name_tag,
+ }
 }
